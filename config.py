2ed13624c19c92372968c6396ded9cad42835f0d
oca <oCa.flash@gmail.com>
2024-10-18 18:51:56 +0300

    fix config USE_ALLOWED_USERS

--- config.py
+++ config.py
@@ -8,6 +8,6 @@ GOOGLE_DRIVE_CREDENTIALS_FILE = os.getenv('GOOGLE_DRIVE_CREDENTIALS_FILE')
 EXCLUDED_FOLDERS = os.getenv('EXCLUDED_FOLDERS').split(',')
 ALLOWED_USERS = list(map(int, os.getenv('ALLOWED_USERS').split(',')))
 MAX_FILE_SIZE_MB = int(os.getenv('MAX_FILE_SIZE_MB'))
-USE_ALLOWED_USERS = os.getenv('USE_ALLOWED_USERS').lower() == 'false'
+USE_ALLOWED_USERS = os.getenv('USE_ALLOWED_USERS', 'False').lower() == 'true'
 STATISTICS_FOLDER = os.getenv('STATISTICS_FOLDER')
 STATISTICS_FILE = os.getenv('STATISTICS_FILE')
--- setup_and_run.sh
+++ setup_and_run.sh
@@ -7,6 +7,12 @@ source venv/bin/activate
 # Обновление pip и установка Poetry
 pip install --upgrade pip
 pip install poetry
+# for replit.com
+# pip install --upgrade python-telegram-bot
+# pip install google-auth google-auth-oauthlib google-auth-httplib2 google-api-python-client
+# pip install python-dotenv
+
+
 
 # Установка зависимостей через Poetry
 poetry install --no-dev --no-root
